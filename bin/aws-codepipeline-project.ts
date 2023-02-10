83219707b5a728d29b4650bc308c4c12eddd9452
Samuel Rojas <frojas@occ.com.mx>
2023-02-09 23:03:25 -0600

    cambios en pipeline CDK

--- bin/aws-codepipeline-project.ts
+++ bin/aws-codepipeline-project.ts
@@ -16,8 +16,8 @@ new AwsCodepipelineProjectStack(app, 'AwsCodepipelineProjectStack', {
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
   env: { 
-    account: '037307398034', // replace with your aws account ID
-    region: 'us-east-1'      // replace with your preferred region
+    account: process.env.CDK_DEFAULT_ACCOUNT, // replace with your aws account ID
+    region: process.env.CDK_DEFAULT_REGION     // replace with your preferred region
   },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
