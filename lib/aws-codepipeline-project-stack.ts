4cdba8192b1a0895adf7bb900c245b6f7b3d902e
Samuel Rojas <frojas@occ.com.mx>
2023-02-09 22:14:05 -0600

    commit inicial

---
+++ lib/aws-codepipeline-project-stack.ts
@@ -0,0 +1,24 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
+
+export class AwsCodepipelineProjectStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+    new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'CDKTestPipeline',       // Creating a new code pipeline which is a construct
+      synth: new ShellStep('Synth', {        // Add a new synthesis 'shellstep' which will be pointed at our gihub repository 
+        input: CodePipelineSource.gitHub('aLLUPS/aws-codepipeline-project', 'main'), // replace the GitHub repository name with 'user-name/repository-name'
+        
+        // The build steps for the pipeline are defined by these commands
+        
+        commands: ['npm ci',
+                   'npm run build',
+                   'npx cdk synth']
+      }),
+    })
+  }
+}
